6d2eaa537c8affb6c14eb9ffe25ee8988e30391d
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-01-31 00:14:21 +0600

    get all keys as an array and all values as an array

---
+++ keys.js
@@ -0,0 +1,24 @@
+const computer = {
+    brand: 'lenovo',
+    price: 35000,
+    processor: 'intel',
+    hdd: '512gb'
+}
+
+/**
+ * --- Object.keys(objName) ---
+ * 
+ * ==> make a array of object keys
+ */
+const KEYs = Object.keys(computer);
+console.log(KEYs);
+console.log('\n');
+
+
+/**
+ * --- Object.values(objName) ---
+ * 
+ * ==> make a array of object values
+ */
+const VALUEs = Object.values(computer);
+console.log(VALUEs);
